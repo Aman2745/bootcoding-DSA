52a285f40191950627f8705b1f23a71a38d979b3
Aman2745 <amandange779@gmail.com>
2023-07-16 13:08:25 +0530

    LeetCode: check palindrome String

---
+++ leetcode_questions/Palindo_String.java
@@ -0,0 +1,27 @@
+package leetcode_questions;
+
+public class Palindo_String {
+    public static boolean ispalindrome(String s){
+
+        s=s.toLowerCase();
+        if(s.length()==0||s==null){
+            return true;
+        }
+
+        for(int i=0;i<s.length()/2;i++){
+            char start=s.charAt(i);
+            char end=s.charAt(s.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String str="abcdcba";
+        System.out.println(ispalindrome(str));
+    }
+}
